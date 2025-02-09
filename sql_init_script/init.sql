3c8dbba7853e56a3663db1c8742c73af0cbcaa92
Jan-Henrik.Capsius <capsiusjanhenrik.bewerbung@gmail.com>
2025-02-09 12:04:22 +0100

    add tables for Pokémon abilities and related dependencies

---
+++ sql_init_script/init.sql
@@ -0,0 +1,48 @@
+-- SQL initialization script
+-- Create teable for the pokemon abilities and sub-information 
+CREATE TABLE
+    IF NOT EXISTS LANGUAGES (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        language VARCHAR(10) NOT NULL
+    );
+
+CREATE TABLE
+    IF NOT EXISTS ALTERNATIVE_ABILITY_NAMES (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        language_id INT NOT NULL,
+        ability_id INT NOT NULL,
+        FOREIGN KEY (language_id) REFERENCES LANGUAGES (id) FOREIGN KEY (ability_id) REFERENCES ABILITIES (id)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS ABILITIES (
+        id INT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        generation_id INT NOT NULL,
+        FOREIGN KEY (generation_id) REFERENCES GENERATRIONS (id)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS GENERATRIONS (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        type VARCHAR(255) NOT NULL
+    );
+
+CREATE TABLE
+    IF NOT EXISTS EFFECT (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        effect VARCHAR(600) NOT NULL,
+        ability_id INT NOT NULL,
+        language_id INT NOT NULL,
+        FOREIGN KEY (ability_id) REFERENCES ABILITIES (id) FOREIGN KEY (language_id) REFERENCES LANGUAGES (id)
+    );
+
+Create table
+    if not exists FLAVOUR_TEXT (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        ability_id INT NOT NULL,
+        language_id INT NOT NULL,
+        FOREIGN KEY (ability_id) REFERENCES ABILITIES (id) FOREIGN KEY (language_id) REFERENCES LANGUAGES (id)
+    );
