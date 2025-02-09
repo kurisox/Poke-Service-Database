342457198b8ccf81dfed7203d0fa183633f3dc99
Jan-Henrik.Capsius <capsiusjanhenrik.bewerbung@gmail.com>
2025-02-09 17:47:38 +0100

    feat: enhance init script to include effect and flavour text SQL statements

--- sql_init_script/init.ts
+++ sql_init_script/init.ts
@@ -3,7 +3,9 @@ import {
     collectUniqueLanguages,
     languagesSQLStatements,
     alternativeNamesSQLStatements,
-    abilitiesSQLStatements
+    abilitiesSQLStatements,
+    effectSQLStatements,
+    flavourTextSqlStatements
   } from "../src/utils/abilities/abilities.ts";
   import { IAbility } from "../src/utils/abilities/IAbility.ts";
   import {
@@ -16,13 +18,16 @@ import {
   import log4js, { Logger } from "log4js";
   import { IGeneration } from "../src/utils/generations/IGeneration.ts";
   
+  //logger setup
   const logger: Logger = log4js.getLogger("init.ts");
   logger.level = "info";
+
   // Get the current directory and file path
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
   const initSqlPath = path.join(__dirname, "init.sql");
   
+  //prepares data for the init.sql script
   async function prepareData() {
     logger.info("Fetching abilities");
     const abilities: IAbility[] = await fetchAbilities();
@@ -36,35 +41,55 @@ import {
     createSQLStatements(abilities, uniqueLanguages, generations);
   }
   
+  //creates the sql statements for the init.sql script
   function createSQLStatements(abilities: IAbility[], languages: Set<string>, generations: IGeneration[]) {
-    const statementArray: string[] = [];
+    const statementArray: Set<string> = new Set();
+    const languagesArray: string[] = Array.from(languages);
     logger.info("Creating language sql statements");
-    const languageStatements: string[] = languagesSQLStatements(languages);
-    statementArray.push(...languageStatements);
+    const languageStatements: Set<string> = languagesSQLStatements(languages);
+    addStatements(statementArray, languageStatements);
     logger.info("Language sql statements created");
     logger.info("Creating alternative names statements");
-    const alternativeNamesStatements: string[] = alternativeNamesSQLStatements(
-      languages,
+    const alternativeNamesStatements: Set<string> = alternativeNamesSQLStatements(
+      languagesArray,
       abilities
     );
-    statementArray.push(...alternativeNamesStatements);
+    addStatements(statementArray, alternativeNamesStatements);
     logger.info("Alternative names statements created");
-    const generationStatements: string[] = generationSQLStatements(generations);
-    statementArray.push(...generationStatements);
-    const abilitiesStatements: string[] = abilitiesSQLStatements(abilities, generations);
-    statementArray.push(...abilitiesStatements);
+    const generationStatements: Set<string> = generationSQLStatements(generations);
+    addStatements(statementArray, generationStatements);
+    logger.info("Creating abilities statements");
+    const abilitiesStatements: Set<string> = abilitiesSQLStatements(abilities, generations);
+    addStatements(statementArray, abilitiesStatements);
+    logger.info("Abilities statements created");
+    logger.info("Creating effect statements");
+    const effectStatements: Set<string> = effectSQLStatements(abilities, languagesArray);
+    addStatements(statementArray, effectStatements);
+    logger.info("Effect statements created");
+    logger.info("Creating flavour text statements");
+    const flavourTextStatements: Set<string> = flavourTextSqlStatements(abilities, languagesArray);
+    addStatements(statementArray, flavourTextStatements);
+    logger.info("Flavour text statements created");
     populateSQLScript(statementArray);
   }
-  
-  function populateSQLScript(statementArray: string[]) {
+
+  function addStatements(statementsArray: Set<string>, statements: Set<string> ) {
+    statements.forEach((statement: string) => statementsArray.add(statement));
+  }
+
+  //populates the init.sql script with the statements
+  function populateSQLScript(statementsArray: Set<string>) {
     logger.info("Populating init.sql with statements");
-    statementArray.forEach((statement: string) => {
+    statementsArray.forEach((statement: string) => {
       fs.appendFileSync(initSqlPath, statement);
     });
     logger.info("init.sql populated with statements");
   
   }
   
-  logger.info("Starting init.ts");
-  prepareData();
-  
+  //start the script
+  function main() {
+    logger.info("Starting init.ts");
+    prepareData();
+  }
+  main();
