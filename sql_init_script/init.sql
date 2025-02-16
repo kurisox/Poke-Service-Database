fe375bab7184e1892579f4cda507199515b4a3cc
Jan-Henrik.Capsius <capsiusjanhenrik.bewerbung@gmail.com>
2025-02-16 19:05:45 +0100

    feat: add initial SQL tables for Pokémon database schema including languages, abilities, and Pokémon details

--- sql_init_script/init.sql
+++ sql_init_script/init.sql
@@ -3,7 +3,9 @@ CREATE DATABASE IF NOT EXISTS poke_service_db;
 
 USE poke_service_db;
 
--- Create for the aviable languages 
+-- base tables
+
+-- Create table for the avaible languages 
 CREATE TABLE
     IF NOT EXISTS LANGUAGES (
         id INT AUTO_INCREMENT,
@@ -27,6 +29,20 @@ CREATE TABLE
         primary key (id)
     );
 
+-- Create table for the pokemon
+Create table
+    if not exists POKEMONS (
+        id INT NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        height DOUBLE NOT NULL,
+        weight DOUBLE NOT NULL,
+        primary key (id)
+    );
+
+
+-- fk tables    
+
+-- Create table for the abilities of the pokemon
 CREATE TABLE
     IF NOT EXISTS ABILITIES (
         id INT NOT NULL,
@@ -35,6 +51,7 @@ CREATE TABLE
         primary key (id)
     );
 
+-- Create table for the alternative names of the abilities
 CREATE TABLE
     IF NOT EXISTS ALTERNATIVE_ABILITY_NAMES (
         id INT AUTO_INCREMENT,
@@ -44,9 +61,9 @@ CREATE TABLE
         primary key (id)
     );
 
-
+-- Create table for the effects of the abilities
 CREATE TABLE
-    IF NOT EXISTS EFFECTS (
+    IF NOT EXISTS ABILITY_EFFECTS (
         id INT AUTO_INCREMENT,
         effect VARCHAR(2000) NOT NULL,
         ability_id INT NOT NULL,
@@ -54,14 +71,26 @@ CREATE TABLE
         primary key (id)
     );
 
+-- Create table for the flavour texts of the abilities
 Create table
-    if not exists FLAVOUR_TEXTS (
+    if not exists ABILITY_FLAVOUR_TEXTS (
         id INT AUTO_INCREMENT,
         flavour_text VARCHAR(255) NOT NULL,
         ability_id INT NOT NULL,
         language_id INT NOT NULL,
         primary key (id)
     );
+
+
+Create table
+    if not exists POKEMON_SPRITES (
+        id INT NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        url VARCHAR(255) NOT NULL,
+        pokemon_id INT NOT NULL,
+        primary key (id)
+    );
+
 
 -- join tables
 Create table
@@ -106,6 +135,20 @@ Create table
         primary key (type_id, reference_type_id)
     );
 
+Create table
+    if not exists POKEMON_ABILITIES (
+        pokemon_id INT NOT NULL,
+        ability_id INT NOT NULL,
+        primary key (pokemon_id, ability_id)
+    );
+
+Create table
+    if not exists POKEMON_TYPES (
+        pokemon_id INT NOT NULL,
+        type_id INT NOT NULL,
+        primary key (pokemon_id, type_id)
+    );    
+
 -- create constraints
 alter table ALTERNATIVE_ABILITY_NAMES add constraint `ALTERNATIVE_ABILITY_NAMES_LANGUAGES` foreign key (language_id) references LANGUAGES (id) on delete cascade;
 
@@ -113,13 +156,13 @@ alter table ALTERNATIVE_ABILITY_NAMES add constraint `ALTERNATIVE_ABILITY_NAMES_
 
 alter table ABILITIES add constraint `ABILITIES_GENERATIONS` foreign key (generation_id) references GENERATIONS (id) on delete cascade;
 
-alter table EFFECTS add constraint `EFFECTS_ABILITIES` foreign key (ability_id) references ABILITIES (id) on delete cascade;
+alter table ABILITY_EFFECTS add constraint `EFFECTS_ABILITIES` foreign key (ability_id) references ABILITIES (id) on delete cascade;
 
-alter table EFFECTS add constraint `EFFECTS_LANGUAGES` foreign key (language_id) references LANGUAGES (id) on delete cascade;
+alter table ABILITY_EFFECTS add constraint `EFFECTS_LANGUAGES` foreign key (language_id) references LANGUAGES (id) on delete cascade;
 
-alter table FLAVOUR_TEXTS add constraint `FLAVOUR_TEXT_ABILITIES` foreign key (ability_id) references ABILITIES (id) on delete cascade;
+alter table ABILITY_FLAVOUR_TEXTS add constraint `FLAVOUR_TEXT_ABILITIES` foreign key (ability_id) references ABILITIES (id) on delete cascade;
 
-alter table FLAVOUR_TEXTS add constraint `FLAVOUR_TEXT_LANGUAGES` foreign key (language_id) references LANGUAGES (id) on delete cascade;
+alter table ABILITY_FLAVOUR_TEXTS add constraint `FLAVOUR_TEXT_LANGUAGES` foreign key (language_id) references LANGUAGES (id) on delete cascade;
 
 alter table NO_DAMAGE_TO add constraint `NO_DAMAGE_TO_TYPES` foreign key (type_id) references TYPES (id) on delete cascade;
 
@@ -145,4 +188,15 @@ alter table DOUBLE_DAMAGE_FROM add constraint `DOUBLE_DAMAGE_FROM_TYPES` foreign
 
 alter table DOUBLE_DAMAGE_FROM add constraint `DOUBLE_DAMAGE_FROM_TYPES2` foreign key (reference_type_id) references TYPES (id) on delete cascade;
 
--- values
+alter table POKEMON_ABILITIES add constraint `POKEMON_ABILITIES_POKEMONS` foreign key (pokemon_id) references POKEMONS (id) on delete cascade;
+
+alter table POKEMON_ABILITIES add constraint `POKEMON_ABILITIES_ABILITIES` foreign key (ability_id) references ABILITIES (id) on delete cascade;
+
+alter table POKEMON_SPRITES add constraint `POKEMON_SPRITES_POKEMONS` foreign key (pokemon_id) references POKEMONS (id) on delete cascade;
+
+alter table POKEMON_TYPES add constraint `POKEMON_TYPES_POKEMONS` foreign key (pokemon_id) references POKEMONS (id) on delete cascade;
+
+alter table POKEMON_TYPES add constraint `POKEMON_TYPES_TYPES` foreign key (type_id) references TYPES (id) on delete cascade;
+
+-- values for the tables
+
